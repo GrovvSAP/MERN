6c95e68aed72ce042fc0f8a0c7bbd894f10597fb
Yaniv <yaniv.idov@grovv.co.il>
2025-02-10 15:06:57 +0200

    posting to the server from the Form

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 
+// Explicitly allow CORS for your frontend
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
+app.use(express.json());
 
 app.post('/register',(req,res)=>{
-    res.json('test ok2');
+    const {username,password} = req.body;
+
+
+    res.json({requestData:{username,password}});
 });
 
 app.listen(4000);
